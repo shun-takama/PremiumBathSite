d1bdc32f067f720342d09259607b3686ae1886e9
SHUN TAKAMA <shun.takama0206@gmail.com>
2026-06-04 06:24:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { initAccordion } from "./modules/accordion.js";
 import { initFadein } from "./modules/fadein.js";
+import { initPagetop } from "./modules/pagetop.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initAccordion();
   initFadein();
+  initPagetop();
 });
